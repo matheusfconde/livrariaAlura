159fd12df46fd9d41bc91743935106b5bee5412d
Matheus <frameworksys.matheus@unidas.com.br>
2019-08-19 17:28:08 -0300

    aula 6.1 finalizado parte 1

--- Alura.WebAPI.WebApp/Api/ListasLeituraController.cs
+++ Alura.WebAPI.WebApp/Api/ListasLeituraController.cs
@@ -4,11 +4,13 @@ using System.Linq;
 using System.Threading.Tasks;
 using Alura.ListaLeitura.Modelos;
 using Alura.ListaLeitura.Persistencia;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Lista = Alura.ListaLeitura.Modelos.ListaLeitura;
 
 namespace Alura.WebAPI.WebApp.Api
 {
+    [Authorize]
     [ApiController]
     [Route("api/[controller]")]
     public class ListasLeituraController : ControllerBase
