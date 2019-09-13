7a4e38072819f0c81c8278005e790c7ceb10e65d
Matheus <frameworksys.matheus@unidas.com.br>
2019-09-13 16:42:17 -0300

    9>21

--- Alura.WebAPI.Api/Controllers/Livros2Controller.cs
+++ Alura.WebAPI.Api/Controllers/Livros2Controller.cs
@@ -69,15 +69,7 @@ namespace Alura.WebAPI.WebApp.Api
             if (ModelState.IsValid)
             {
                 var livro = model.ToLivro();
-                try
-                {
-                    _repo.Incluir(livro);
-                }
-                catch (Exception e )
-                {
-                    var errorResponse = ErrorResponse.From(e);
-                    return StatusCode(500, errorResponse);
-                }
+                _repo.Incluir(livro);                
                 var uri = Url.Action("Recuperar", new { id = livro.Id });
                 return Created(uri, livro); //201
             }
